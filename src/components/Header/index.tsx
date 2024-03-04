cdec53efad09cfa013000a233229c2e8f0c472fc
Joao Claudio Pansani <pansanijoaomendonca@gmail.com>
2024-03-04 18:55:32 -0300

    finish coding UI of the project

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer, HeaderContent } from "./styles";
 import Logo from "../../assets/logo-dt-money.svg";
 
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={Logo} />
-        <button>Nova Transacao</button>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <button>Nova Transacao</button>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,40 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp } from "@phosphor-icons/react";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+      <Content>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+        <CloseButton>X</CloseButton>
+
+        <form>
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={22} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={22} />
+              Saida
+            </TransactionTypeButton>
+          </TransactionType>
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  );
+}
